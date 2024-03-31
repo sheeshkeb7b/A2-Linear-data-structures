1798ba2eaf1de379ceb0c457723ca7d65b62537f
Ghaya-aljab <159114408+Ghaya-aljab@users.noreply.github.com>
2024-03-31 20:15:48 +0400

    Update Assignment 2: Linear data structures.py

--- Assignment 2: Linear data structures.py
+++ Assignment 2: Linear data structures.py
@@ -7,7 +7,53 @@ class Patient:
         self.medical_history = medical_history if medical_history else []
         self.current_condition = current_condition if current_condition else ""
         self.appointment = appointment
+        self.prescriptions = Stack()
 
+    # Getter methods
+    def get_patient_id(self):
+        return self.patient_id
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def get_gender(self):
+        return self.gender
+
+    def get_medical_history(self):
+        return self.medical_history
+
+    def get_current_condition(self):
+        return self.current_condition
+
+    def get_appointment(self):
+        return self.appointment
+
+    def get_prescriptions(self):
+        return self.prescriptions
+
+    # Setter methods
+    def set_name(self, name):
+        self.name = name
+
+    def set_age(self, age):
+        self.age = age
+
+    def set_gender(self, gender):
+        self.gender = gender
+
+    def set_medical_history(self, medical_history):
+        self.medical_history = medical_history
+
+    def set_current_condition(self, current_condition):
+        self.current_condition = current_condition
+
+    def set_appointment(self, appointment):
+        self.appointment = appointment
+
+    # Other methods
     def update_medical_history(self, new_history):
         self.medical_history.append(new_history)
 
@@ -16,6 +62,9 @@ class Patient:
 
     def schedule_appointment(self, appointment):
         self.appointment = appointment
+
+    def add_prescription(self, prescription):
+        self.prescriptions.push(prescription)
 
     def __str__(self):
         return f"Patient ID: {self.patient_id}, Name: {self.name}, Age: {self.age}, Gender: {self.gender}"
